c86f283a29ac9a3d0d058d53f203a1b37414f321
Shoxrux Code <shoxruxpl04@gmail.com>
2024-01-29 01:35:30 -0500

    smth

---
+++ database.js
@@ -0,0 +1,12 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
+
+pool.connect((err) => {
+    if(err) throw err
+    console.log('Connected!');
+})
